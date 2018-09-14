164e6e808e8cdcb08c0e8ab9b36c7b8f71880a55
Scott Taylor <scott@adcockpe.com>
2018-09-15 06:16:08 +1000

    Updated to support mset/mget, promises for del, ttyl and reset

--- dist/index.js
+++ dist/index.js
@@ -53,6 +53,67 @@ var redisStore = function redisStore() {
         }
       });
     },
+    mset: function mset() {
+      for (var _len2 = arguments.length, args = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+        args[_key2] = arguments[_key2];
+      }
+
+      var _this = this;
+
+      return new Promise(function (resolve, reject) {
+        var cb = void 0;
+        var options = {};
+
+        if (typeof args[args.length - 1] === 'function') {
+          cb = args.pop();
+        }
+
+        if (args[args.length - 1] instanceof Object && args[args.length - 1].constructor === Object) {
+          options = args.pop();
+        }
+
+        if (!cb) {
+          cb = function cb(err, result) {
+            return err ? reject(err) : resolve(result);
+          };
+        }
+
+        var ttl = options.ttl || options.ttl === 0 ? options.ttl : storeArgs.ttl;
+
+        var multi = void 0;
+        if (ttl) {
+          multi = redisCache.multi();
+        }
+
+        var key = void 0;
+        var value = void 0;
+        var parsed = [];
+        for (var i = 0; i < args.length; i += 2) {
+          key = args[i];
+          value = args[i + 1];
+
+          /**
+           * Make sure the value is cacheable
+           */
+          if (!_this.isCacheableValue(value)) {
+            return cb(new Error('value cannot be ' + value));
+          }
+
+          value = JSON.stringify(value) || '"undefined"';
+          parsed.push.apply(parsed, [key, value]);
+
+          if (ttl) {
+            multi.setex(key, ttl, value);
+          }
+        }
+
+        if (ttl) {
+          multi.exec(handleResponse(cb));
+        } else {
+          redisCache.mset.apply(redisCache, [].concat(parsed, [handleResponse(cb)]));
+        }
+      });
+    },
     get: function get(key, options, cb) {
       return new Promise(function (resolve, reject) {
         if (typeof options === 'function') {
@@ -65,20 +126,79 @@ var redisStore = function redisStore() {
           };
         }
 
-        redisCache.get(key, handleResponse(cb, { parse: true }));
+        redisCache.get(key, handleResponse(cb, {
+          parse: true
+        }));
       });
     },
-    del: function del(key, options, cb) {
-      if (typeof options === 'function') {
-        cb = options;
+    mget: function mget() {
+      for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
+        args[_key3] = arguments[_key3];
       }
 
-      redisCache.del(key, handleResponse(cb));
+      return new Promise(function (resolve, reject) {
+        var cb = void 0;
+        var options = {};
+
+        if (typeof args[args.length - 1] === 'function') {
+          cb = args.pop();
+        }
+
+        if (args[args.length - 1] instanceof Object && args[args.length - 1].constructor === Object) {
+          options = args.pop();
+        }
+
+        if (!cb) {
+          cb = function cb(err, result) {
+            return err ? reject(err) : resolve(result);
+          };
+        }
+
+        redisCache.mget.apply(redisCache, [].concat(args, [handleResponse(cb, {
+          parse: true
+        })]));
+      });
+    },
+    del: function del() {
+      for (var _len4 = arguments.length, args = Array(_len4), _key4 = 0; _key4 < _len4; _key4++) {
+        args[_key4] = arguments[_key4];
+      }
+
+      return new Promise(function (resolve, reject) {
+        var cb = void 0;
+        var options = {};
+
+        if (typeof args[args.length - 1] === 'function') {
+          cb = args.pop();
+        }
+
+        if (args[args.length - 1] instanceof Object && args[args.length - 1].constructor === Object) {
+          options = args.pop();
+        }
+
+        if (!cb) {
+          cb = function cb(err, result) {
+            return err ? reject(err) : resolve(result);
+          };
+        }
+
+        redisCache.del.apply(redisCache, [].concat(args, [handleResponse(cb)]));
+      });
     },
     reset: function reset(cb) {
-      return redisCache.flushdb(handleResponse(cb));
+      return new Promise(function (resolve, reject) {
+        if (!cb) {
+          cb = function cb(err, result) {
+            return err ? reject(err) : resolve(result);
+          };
+        }
+
+        redisCache.flushdb(handleResponse(cb));
+      });
     },
-    keys: function keys(pattern, cb) {
+    keys: function keys() {
+      var pattern = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '*';
+      var cb = arguments[1];
       return new Promise(function (resolve, reject) {
         if (typeof pattern === 'function') {
           cb = pattern;
@@ -95,9 +215,17 @@ var redisStore = function redisStore() {
       });
     },
     ttl: function ttl(key, cb) {
-      return redisCache.ttl(key, handleResponse(cb));
+      return new Promise(function (resolve, reject) {
+        if (!cb) {
+          cb = function cb(err, result) {
+            return err ? reject(err) : resolve(result);
+          };
+        }
+
+        redisCache.ttl(key, handleResponse(cb));
+      });
     },
-    isCacheableValue: storeArgs.isCacheableValue || function (value) {
+    isCacheableValue: storeArgs.is_cacheable_value || function (value) {
       return value !== undefined && value !== null;
     }
   };
@@ -112,11 +240,21 @@ function handleResponse(cb) {
     }
 
     if (opts.parse) {
-      try {
-        result = JSON.parse(result);
-      } catch (e) {
-        return cb && cb(e);
+      var isMultiple = Array.isArray(result);
+      if (!isMultiple) {
+        result = [result];
       }
+
+      result = result.map(function (_result) {
+        try {
+          _result = JSON.parse(_result);
+        } catch (e) {
+          return cb && cb(e);
+        }
+        return _result;
+      });
+
+      result = isMultiple ? result : result[0];
     }
 
     return cb && cb(null, result);
